64df57fbb9bdcbb557d99c0bd60688020a1673af
Bolum Victor <vbolum@epayplusng.com>
2018-11-02 14:19:22 +0100

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+import { ListPage } from './../list/list';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FingerprintAIO,FingerprintOptions } from "@ionic-native/fingerprint-aio";
+import { ToastController } from "ionic-angular";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  fingerprintOptions : FingerprintOptions;
 
+  constructor(public navCtrl: NavController, public faio:FingerprintAIO, public toast:ToastController) {
+    this.fingerprintOptions = {
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', //Only necessary for Android
+      disableBackup:false,  //Only for Android(optional)
+    }
   }
+  
+
+  showFingerprint(){
+    let result = this.faio.isAvailable().then((res)=>{
+      alert("success - " + res);
+      this.faio.show(this.fingerprintOptions).then((res)=>{
+        this.navCtrl.push(ListPage)
+      }).catch((err)=>{
+        alert("error - " + err);
+      });
+    }).catch((err)=>{
+      alert('error '+ err)
+  });
+    
+  }
+
+  
 
 }
